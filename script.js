e1d232618e539b0307634e3389a15cb19cf4207b
nozky <norvillie.villaruel@edu.sait.ca>
2021-10-04 00:46:28 -0600

    first condition solve

--- script.js
+++ script.js
@@ -4,29 +4,48 @@
 // Then figure out how to make random divs on each load (with same exit space to middle-right)
 //and add Completed, retry or home button ( also add homepage with start and exit button )
 
+// im lazy
+const getId =(id)=>{
+    return document.getElementById(id)
+}
+
+const message = getId("message")
+let prevParent = null
 
 
+// event- drag over
 function allowDrop(ev) {
-    notAllowed = document.getElementById("div3").innerHTML
-
-    if (document.getElementById("text") != notAllowed) {
-
-        ev.preventDefault();
+    // do not allow if the div child is more than 1
+    if( ev.target.childElementCount < 1){
+        ev.preventDefault()
     }
 }
 
+
+// Start drap
 function drag(ev) {
-
-
-    ev.dataTransfer.setData("text", ev.target.id);
+    // store prevState / prev parent of the pill
+    prevParent = ev.target.parentNode.id
+    ev.dataTransfer.setData("pill", ev.target.id);
 }
 
 
+// on drop
 function drop(ev) {
-
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
 
+    const data = ev.dataTransfer.getData("pill");
+    if(ev.target.childElementCount < 1 ){
+      ev.target.appendChild(getId(data));
+    }else{
+        getId(prevParent).appendChild(getId(data))
+    }
+
+    //do not allow drop over image 
+    if(ev.target.localName === "img"){
+        getId("message").innerText ="Your not allowed to drop pill on another pill"
+        getId(prevParent).appendChild(getId(ev.dataTransfer.getData("pill")))
+    }
+    
 }
 
