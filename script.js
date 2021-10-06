f930a9f0fe81b3b8474f4955ffd552362a8a99f0
nozky <norvillie.villaruel@edu.sait.ca>
2021-10-06 17:49:32 -0600

    clean up

--- script.js
+++ script.js
@@ -11,29 +11,46 @@ const getId =(id)=>{
 
 const message = getId("message")
 let prevParent = null
+let prevNumber = null
+let preventOverPill = false
 
 
-// event- drag over
+// event- dragOver
 function allowDrop(ev) {
     // do not allow if the div child is more than 1
     if( ev.target.childElementCount < 1){
         ev.preventDefault()
     }
+
+    console.log("drag over", ev.target.id)
+    if(ev.target.id === "drag2"){
+        preventOverPill = true
+        console.log('true')
+    } else{
+        preventOverPill = false
+    }
+
 }
 
 
-// Start drag
+// Start startDrag
 function drag(ev) {
+    //clear msg when star dragging
+    getId("message").innerText =""
+    
     // store prevState / prev parent of the pill
     prevParent = ev.target.parentNode.id
     ev.dataTransfer.setData("pill", ev.target.id);
+    prevNumber = ev.target.parentNode.getAttribute('value')
+    console.log(prevNumber)
 }
 
 
-// on drop
+// on onDrop
 function drop(ev) {
     ev.preventDefault();
 
+    //prevent dropping it on non empty div
     const data = ev.dataTransfer.getData("pill");
     if(ev.target.childElementCount < 1 ){
       ev.target.appendChild(getId(data));
@@ -41,7 +58,7 @@ function drop(ev) {
         getId(prevParent).appendChild(getId(data))
     }
 
-    //do not allow drop over image 
+    //do not allow drop over image/pill 
     if(ev.target.localName === "img"){
         getId("message").innerText ="Your not allowed to drop pill on another pill"
         getId(prevParent).appendChild(getId(ev.dataTransfer.getData("pill")))
